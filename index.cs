f1d3e28dae8835d556ce9344b2e58c9f64ac59ee
murilocoutinho1 <omccoutin@gmail.com>
2025-08-14 22:05:02 -0300

    primeira aula, cálculo de imc

---
+++ index.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace App2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float weight, height, imc;
+            weight = 86;
+            height = 1.71F;
+            imc = weight / (height * height);
+            WriteLine("O resultado foi: " + imc);
+            ReadKey();
+        }
+    }
+}
